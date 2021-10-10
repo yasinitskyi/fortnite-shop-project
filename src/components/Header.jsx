82a551f41332853086e7d33e4d7f999ce100add0
Yurii Ya <yurii.ya.dipp@gmail.com>
2021-10-10 19:05:57 +0800

    Added: homepage link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 	return (
 		<nav>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React shop</a>
+      <a href="/" className="brand-logo">React shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="https://github.com/yasinitskyi/fortnite-shop-project" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
