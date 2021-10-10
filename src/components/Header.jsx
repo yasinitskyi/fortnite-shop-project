683827381184ee66aa2497c879edcfed715dcff4
Yurii Ya <yurii.ya.dipp@gmail.com>
2021-10-10 19:05:24 +0800

    Added: repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     <div className="nav-wrapper">
       <a href="#" className="brand-logo">React shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repo</a></li>
+        <li><a href="https://github.com/yasinitskyi/fortnite-shop-project" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
     </div>
   </nav>
